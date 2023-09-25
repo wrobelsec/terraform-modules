8acf3c88e6309d4acccb474fbfc2574c09798c6f
Wrobelsec <a@wrobelsec.com>
2023-09-25 01:08:40 -0500

    ran terraform fmt

--- new-aws-account/locals.tf
+++ new-aws-account/locals.tf
@@ -1,7 +1,7 @@
 locals {
-    console_group_name = "${var.account_alias}_console"
-    admin_group_name = "${var.account_alias}_admin"
-    users = [
+  console_group_name = "${var.account_alias}_console"
+  admin_group_name   = "${var.account_alias}_admin"
+  users = [
     {
       "name" : "${var.account_alias}_admin",
       "groups" : [local.console_group_name, local.admin_group_name]
